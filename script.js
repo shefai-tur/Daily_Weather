371212d72fc4994eec8f2538dd6ab41c7463c9cd
shefaitur <shefaitur@gmail.com>
2024-07-11 03:14:13 -0700

    error add

--- script.js
+++ script.js
@@ -4,41 +4,34 @@ const APIUrl = "http://api.weatherapi.com/v1/current.json?&q=";
 //const apiUrl ="http://api.weatherapi.com/v1/current.json?key=6301961cee4942729f9105339241007&q="
 //const searchBox = "Bangladesh";
 
-
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   // const response = await fetch(apiUrl+city)
   const response = await fetch(APIUrl + city + `&key=${apiKey}`);
-  var data = await response.json();
-  console.log(data);
-  document.querySelector(".city").innerHTML = data.location.name;
-  document.querySelector(".temp").innerHTML =
-    Math.round(data.current.temp_c) + "°c";
-  document.querySelector(".humidity").innerHTML = data.current.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.current.wind_kph + "km/h";
-  document.querySelector(".weather-text").innerHTML =data.current.condition.text;
+  if (response.status == 400) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.location.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.current.temp_c) + "°c";
+    document.querySelector(".humidity").innerHTML = data.current.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.current.wind_kph + "km/h";
+    document.querySelector(".weather-text").innerHTML =
+      data.current.condition.text;
 
- 
-  //console.log("test->", data.current.condition.icon);
-//   let icon = data.current.condition.icon;
- 
-
-// //   img.src = icon;
-// img.src = 'icon';
-// document.body.appendChild(img);
-
-// var imageUrl = data.current.condition.icon;
-// var img = new Image();
-// img.src = imageUrl;
-// img.alt = 'Weather condition';
-
-
- 
+    var imageUrl = data.current.condition.icon;
+    weatherIcon.src = imageUrl;
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
 }
 
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
-// checkWeather(searchBox);
